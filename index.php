f34a91c2f3261c51cf1fd1d12fb7fd237ce9b39e
Dimitar Mitev <dimitar.mitev@experian.com>
2020-11-23 17:48:42 +0200

    added bootstrap like theme

--- index.php
+++ index.php
@@ -2,12 +2,6 @@
 function adminer_object() {
 	// required to run any plugin
     include_once "./plugins/plugin.php";
-    include_once './plugins/designs.php';
-
-    $designs = array();
-	foreach (glob("./designs/*", GLOB_ONLYDIR) as $filename) {
-		$designs["$filename/adminer.css"] = basename($filename);
-	}
 	
 	// autoloader
 	foreach (glob("./plugins/*.php") as $filename) {
@@ -15,14 +9,15 @@ function adminer_object() {
 	}
 	
 	$plugins = array(
+        new AdminerTheme(),
+
 		// specify enabled plugins here
 		new AdminerDatabaseHide(array('information_schema')),
 		new AdminerDumpJson,
 		new AdminerDumpBz2,
 		new AdminerDumpZip,
-		new AdminerDumpXml,
+        new AdminerDumpXml,
         new AdminerDumpAlter,
-        new AdminerDesigns($designs),
 		//~ new AdminerSqlLog("past-" . rtrim(`git describe --tags --abbrev=0`) . ".sql"),
 		//~ new AdminerEditCalendar(script_src("../externals/jquery-ui/jquery-1.4.4.js") . script_src("../externals/jquery-ui/ui/jquery.ui.core.js") . script_src("../externals/jquery-ui/ui/jquery.ui.widget.js") . script_src("../externals/jquery-ui/ui/jquery.ui.datepicker.js") . script_src("../externals/jquery-ui/ui/jquery.ui.mouse.js") . script_src("../externals/jquery-ui/ui/jquery.ui.slider.js") . script_src("../externals/jquery-timepicker/jquery-ui-timepicker-addon.js") . "<link rel='stylesheet' href='../externals/jquery-ui/themes/base/jquery.ui.all.css'>\n<style>\n.ui-timepicker-div .ui-widget-header { margin-bottom: 8px; }\n.ui-timepicker-div dl { text-align: left; }\n.ui-timepicker-div dl dt { height: 25px; }\n.ui-timepicker-div dl dd { margin: -25px 0 10px 65px; }\n.ui-timepicker-div td { font-size: 90%; }\n</style>\n", "../externals/jquery-ui/ui/i18n/jquery.ui.datepicker-%s.js"),
 		//~ new AdminerTinymce("../externals/tinymce/jscripts/tiny_mce/tiny_mce_dev.js"),
@@ -34,7 +29,8 @@ function adminer_object() {
 		new AdminerForeignSystem,
 		new AdminerEnumOption,
 		new AdminerTablesFilter,
-		new AdminerEditForeign,
+        new AdminerEditForeign,
+        new AdminerPrettyJsonColumn
 	);
 	
 	/* It is possible to combine customization and plugins:
